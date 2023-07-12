59c9951165fa7627efae2b5dacab4cad4a0a5646
Thiaguin2004 <89226093+Thiaguin2004@users.noreply.github.com>
2023-07-12 11:01:30 -0300

    Adicionar arquivos de projeto.

---
+++ OpenRobotics/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OpenRobotics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
